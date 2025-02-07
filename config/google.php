838890f5e4ee1e46f111667c65f00061411854de
Mite Stojanov <stojanov1990@gmail.com>
2025-02-07 20:35:08 +0100

    update google to use env

--- config/google.php
+++ config/google.php
@@ -2,9 +2,9 @@
 // config/google.php
 
 return [
-    'client_id'     => 'm.apps.googleusercontent.com',
-    'client_secret' => 'LI',
-    'redirect_uri'  => 'http://localhost:8888/auth/google/callback',
+    'client_id'     => $_ENV['GOOGLE_CLIENT_ID'] ?? '',
+    'client_secret' => $_ENV['GOOGLE_CLIENT_SECRET'] ?? '',
+    'redirect_uri'  => $_ENV['GOOGLE_REDIRECT_URI'] ?? '',
     'scopes'        => [
         'email',
         'profile',
