0defb7e0a38861f7f14c459663001f433fac48dc
Mite Stojanov <stojanov1990@gmail.com>
2025-02-08 17:19:41 +0100

    fix phinx

--- phinx.php
+++ phinx.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/constants.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config/constants.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
