283193cbc734bfbaaaf507058a8dcdca42ad11e0
Mite Stojanov <stojanov1990@gmail.com>
2025-02-08 01:36:30 +0100

    update index

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 use App\Controllers\InvoiceController;
 use App\Controllers\AuthController;
 
